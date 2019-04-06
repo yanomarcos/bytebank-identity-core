7982b934387621f08623ad372132a2df7bff1f51
yanomarcos <yanomarcos@gmail.com>
2019-04-06 08:49:07 -0300

    identityDbContext e identityUser

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -1,4 +1,6 @@
-﻿using ByteBank.Forum.ViewModels;
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,7 +20,23 @@ namespace ByteBank.Forum.Controllers
         [HttpPost]
         public ActionResult Registrar(ContaRegistrarViewModel modelo)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>();
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                dbContext.Users.Add(novoUsuario);
+                dbContext.SaveChanges();
+
+                //podemos incluir usuario
+                return RedirectToAction("Index", "Home");
+            }
+            //alguma coisa de errado aconteceu
+            return View(modelo);
         }
     }
 }
