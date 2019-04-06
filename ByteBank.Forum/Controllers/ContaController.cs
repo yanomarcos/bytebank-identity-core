ee1ca5d434e3f388839736c3e314fd043718a73b
yanomarcos <yanomarcos@gmail.com>
2019-04-06 08:11:50 -0300

    Add project files.

---
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Forum.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
